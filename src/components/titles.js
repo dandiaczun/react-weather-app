ef848062e27536a08e71e4b7712000f4640b02af
Dan Diaczun <dandiacz@uschizog8640.global.publicisgroupe.net>
2019-07-24 13:48:47 -0500

    adding component files

---
+++ src/components/titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Titles = (props) => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p> Look up the weather in any city </p>
+    </div>
+  )
+}
+export default Titles;
